fd6ac081c426fb5b3178edbe967df46c8d43cb96
David Ly <david@oncallhealth.com>
2024-08-12 13:03:58 -0400

    allow building on iOS 14

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
   name: "swift-markdown-ui",
   platforms: [
     .macOS(.v12),
-    .iOS(.v15),
+    .iOS(.v14),
     .tvOS(.v15),
     .macCatalyst(.v15),
     .watchOS(.v8),
