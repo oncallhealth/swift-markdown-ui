93fec5f936caa7ce5dda453acb075849ee5b353f
David Ly <david@oncallhealth.com>
2024-08-12 13:20:46 -0400

    target 13

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
   name: "swift-markdown-ui",
   platforms: [
     .macOS(.v12),
-    .iOS(.v14),
+    .iOS(.v13),
     .tvOS(.v15),
     .macCatalyst(.v15),
     .watchOS(.v8),
